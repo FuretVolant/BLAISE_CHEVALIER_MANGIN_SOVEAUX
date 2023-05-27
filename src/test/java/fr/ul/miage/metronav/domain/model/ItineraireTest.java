6744c8cfcb02f4297867a04e79a710bdbe6566ce
switch-bls <aurelien.bls88@gmail.com>
2023-05-27 15:22:49 +0200

    Implementation Service Station

--- src/test/java/fr/ul/miage/metronav/domain/model/ItineraireTest.java
+++ src/test/java/fr/ul/miage/metronav/domain/model/ItineraireTest.java
@@ -1,8 +1,6 @@
 package fr.ul.miage.metronav.domain.model;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,9 +17,9 @@ public class ItineraireTest {
         Itineraire itineraire = new Itineraire(trajets);
 
         // Création d'une station de départ
-        Station stationDepart = new Station("Gare Centrale", 5, Arrays.asList(new Ligne("L1")), null);
+        Station stationDepart = new Station("Gare Centrale", new Position(-1.0, 20.0),5, Arrays.asList(new Ligne("L1")), null);
         // Création d'une station d'arrrivée
-        Station stationArrivee = new Station("Gare du Nord", 10, Arrays.asList(new Ligne("L1")), null);
+        Station stationArrivee = new Station("Gare du Nord", new Position(10.0, -2.0),10, Arrays.asList(new Ligne("L1")), null);
 
 
         // Création d'un trajet
@@ -39,18 +37,18 @@ public class ItineraireTest {
     public void testGetTempsTotal() {
 
         // Création d'une station de départ
-        Station stationDepart1 = new Station("Gare Centrale", 5, Arrays.asList(new Ligne("L1")), null);
+        Station stationDepart1 = new Station("Gare Centrale",new Position(1.0, 2.0), 5, Arrays.asList(new Ligne("L1")), null);
         // Création d'une station d'arrrivée
-        Station stationArrivee1 = new Station("Gare du Nord", 20, Arrays.asList(new Ligne("L1")), null);
+        Station stationArrivee1 = new Station("Gare du Nord",new Position(3.0, 2.0), 20, Arrays.asList(new Ligne("L1")), null);
 
 
         // Création d'un trajet
         Trajet trajet1 = new Trajet(stationDepart1,stationArrivee1,4);
 
         // Création d'une station de départ
-        Station stationDepart2 = new Station("Gare du Sud", 7, Arrays.asList(new Ligne("L1")), null);
+        Station stationDepart2 = new Station("Gare du Sud",new Position(4.0, 4.0), 7, Arrays.asList(new Ligne("L1")), null);
         // Création d'une station d'arrrivée
-        Station stationArrivee2 = new Station("Gare de l'Est", 10, Arrays.asList(new Ligne("L1")), null);
+        Station stationArrivee2 = new Station("Gare de l'Est",new Position(8.0, 2.0), 10, Arrays.asList(new Ligne("L1")), null);
 
 
         // Création d'un trajet
---
+++ src/test/java/fr/ul/miage/metronav/domain/service/StationServiceTest.java
@@ -0,0 +1,57 @@
+package fr.ul.miage.metronav.domain.service;
+import fr.ul.miage.metronav.domain.model.Position;
+import fr.ul.miage.metronav.domain.model.Station;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StationServiceTest {
+
+    @Test
+    public void testTrouverPlusProcheStationVide() {
+        // Position de référence pour les tests
+        Position position = new Position(0.0, 0.0);
+        // Test lorsque la liste des stations est vide
+        List<Station> listeStationsVide = new ArrayList<>();
+        Station stationPlusProcheVide = new StationServiceImpl().trouverPlusProcheStation(position, listeStationsVide);
+        Assertions.assertNull(stationPlusProcheVide);
+    }
+
+    @Test
+    public void testTrouverPlusProcheStationUnique() {
+        // Position de référence pour les tests
+        Position position = new Position(0.0, 0.0);
+
+        List<Station> listeStationsUnique = new ArrayList<>();
+        // Test avec une seule station dans la liste
+        Station stationUnique = new Station("Station Unique", new Position(10.0, 10.0), 5, new ArrayList<>(), "");
+        listeStationsUnique.add(stationUnique);
+        Station stationPlusProcheUnique = new StationServiceImpl().trouverPlusProcheStation(position, listeStationsUnique);
+        Assertions.assertEquals(stationUnique, stationPlusProcheUnique);
+    }
+
+    @Test
+    public void testTrouverPlusProcheStationList() {
+        // Création de quelques stations pour les tests
+        Station stationA = new Station("Station A", new Position(-1.0, 2.0), 3, new ArrayList<>(), "");
+        Station stationB = new Station("Station B", new Position(3.0, 4.0), 2, new ArrayList<>(), "");
+        Station stationC = new Station("Station C", new Position(-5.0, 6.0), 3, new ArrayList<>(), "");
+        Station stationD = new Station("Station D", new Position(7.0, 8.0), 4, new ArrayList<>(), "");
+
+        List<Station> listeStations = new ArrayList<>();
+        listeStations.add(stationA);
+        listeStations.add(stationB);
+        listeStations.add(stationC);
+        listeStations.add(stationD);
+
+        // Position de référence pour les tests
+        Position position = new Position(0.0, 0.0);
+
+        // Test avec plusieurs stations dans la liste
+        Station stationPlusProche = new StationServiceImpl().trouverPlusProcheStation(position, listeStations);
+        Assertions.assertEquals(stationA, stationPlusProche);
+
+    }
+}
