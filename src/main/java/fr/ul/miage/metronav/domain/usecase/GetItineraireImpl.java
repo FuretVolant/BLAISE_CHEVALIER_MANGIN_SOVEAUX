74ca52494a3a1a8f4cfdf12c039c2b8353c479a3
switch-bls <aurelien.bls88@gmail.com>
2023-05-29 18:36:42 +0200

    add Use get Itineraire

--- src/main/java/fr/ul/miage/metronav/domain/usecase/GetItineraireImpl.java
+++ src/main/java/fr/ul/miage/metronav/domain/usecase/GetItineraireImpl.java
@@ -4,28 +4,40 @@ import fr.ul.miage.metronav.data.StationRepository;
 import fr.ul.miage.metronav.data.TrajetRepository;
 import fr.ul.miage.metronav.domain.model.Itineraire;
 import fr.ul.miage.metronav.domain.model.Position;
-import fr.ul.miage.metronav.domain.model.Trajet;
-
-import java.util.List;
+import fr.ul.miage.metronav.domain.model.Station;
+import fr.ul.miage.metronav.domain.service.ServiceItineraire;
+import fr.ul.miage.metronav.domain.service.ServiceStation;
 
 public class GetItineraireImpl implements GetItineraire{
 
     StationRepository stationRepository;
     TrajetRepository trajetRepository;
+    ServiceStation serviceStation;
+    ServiceItineraire serviceItineraire;
 
-    public GetItineraireImpl(StationRepository stationRepository, TrajetRepository trajetRepository){
+
+    public GetItineraireImpl(StationRepository stationRepository, TrajetRepository trajetRepository, ServiceStation serviceStation, ServiceItineraire serviceItineraire){
         this.stationRepository = stationRepository;
         this.trajetRepository = trajetRepository;
+        this.serviceStation = serviceStation;
+        this.serviceItineraire = serviceItineraire;
     }
 
     @Override
     public Itineraire getItineraireRapide(Position depart, Position arrivee) {
-        return null;
+
+        Station stationDepart = serviceStation.trouverPlusProcheStation(depart, stationRepository.getAllStation());
+        Station stationArrivee = serviceStation.trouverPlusProcheStation(arrivee, stationRepository.getAllStation());
+
+        return serviceItineraire.calculerItineraireRapide(stationDepart,  stationArrivee,  stationRepository.getAllStation(), trajetRepository.getAllTrajet());
     }
 
     @Override
     public Itineraire getItineraireSimple(Position depart, Position arrivee) {
-        return null;
+        Station stationDepart = serviceStation.trouverPlusProcheStation(depart, stationRepository.getAllStation());
+        Station stationArrivee = serviceStation.trouverPlusProcheStation(arrivee, stationRepository.getAllStation());
+
+        return serviceItineraire.calculerItineraireSimple(stationDepart,  stationArrivee,  stationRepository.getAllStation(), trajetRepository.getAllTrajet());
     }
 
     @Override
---
+++ src/test/java/fr/ul/miage/metronav/domain/usecase/GetItineraireImplTest.java
@@ -0,0 +1,66 @@
+package fr.ul.miage.metronav.domain.usecase;
+import fr.ul.miage.metronav.data.StationLocalRepositoryImpl;
+import fr.ul.miage.metronav.data.TrajetLocalRepositoryImpl;
+import fr.ul.miage.metronav.domain.model.Itineraire;
+import fr.ul.miage.metronav.domain.model.Position;
+import fr.ul.miage.metronav.domain.model.Station;
+import fr.ul.miage.metronav.domain.model.Trajet;
+import fr.ul.miage.metronav.domain.service.ServiceItineraire;
+import fr.ul.miage.metronav.domain.service.ServiceItineraireImpl;
+import fr.ul.miage.metronav.domain.service.ServiceStation;
+import fr.ul.miage.metronav.domain.service.ServiceStationImpl;
+import fr.ul.miage.metronav.util.MetroGraph;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetItineraireImplTest {
+
+    @Test
+    public void getRapideItinéraireTest(){
+
+        MetroGraph metroGraph = new MetroGraph();
+        StationLocalRepositoryImpl stationRepository = new StationLocalRepositoryImpl();
+        stationRepository.setStationList(metroGraph.getStationList());
+        TrajetLocalRepositoryImpl trajetRepository = new TrajetLocalRepositoryImpl();
+        trajetRepository.setTrajetList(metroGraph.getTrajetList());
+
+        ServiceItineraire serviceItineraire = new ServiceItineraireImpl();
+
+        ServiceStation serviceStation = new ServiceStationImpl();
+
+        GetItineraire getItineraireUseCase = new GetItineraireImpl(stationRepository, trajetRepository, serviceStation, serviceItineraire);
+
+        Position depart = new Position(0.0, 0.0);
+        Position arrivee = new Position(0.0, 6.0);
+
+
+        Itineraire i = getItineraireUseCase.getItineraireRapide(depart, arrivee);
+        assertEquals("[[Station A : 5 : Station D : 4], [Station D : 4 : Station E : 4], [Station E : 4 : Station F : 4], [Station C : 7 : Station F : 4], [Station G : 4 : Station C : 7], [Station G : 4 : Station H : 4]]", i.getTrajetList().toString());
+    }
+
+    @Test
+    public void getSimpleItinéraireTest(){
+
+        MetroGraph metroGraph = new MetroGraph();
+        StationLocalRepositoryImpl stationRepository = new StationLocalRepositoryImpl();
+        stationRepository.setStationList(metroGraph.getStationList());
+        TrajetLocalRepositoryImpl trajetRepository = new TrajetLocalRepositoryImpl();
+        trajetRepository.setTrajetList(metroGraph.getTrajetList());
+
+        ServiceItineraire serviceItineraire = new ServiceItineraireImpl();
+
+        ServiceStation serviceStation = new ServiceStationImpl();
+
+        GetItineraire getItineraireUseCase = new GetItineraireImpl(stationRepository, trajetRepository, serviceStation, serviceItineraire);
+
+        Position depart = new Position(0.0, 0.0);
+        Position arrivee = new Position(0.0, 6.0);
+
+
+        Itineraire i = getItineraireUseCase.getItineraireSimple(depart, arrivee);
+        assertEquals("[[Station A : 5 : Station B : 4], [Station B : 4 : Station C : 7], [Station C : 7 : Station F : 4], [Station G : 4 : Station F : 4], [Station G : 4 : Station H : 4]]", i.getTrajetList().toString());
+    }
+}
