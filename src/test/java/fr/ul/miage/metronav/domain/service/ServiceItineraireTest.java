4a5915a5010c4bb6ad30a25093e366ab5e951e39
switch-bls <aurelien.bls88@gmail.com>
2023-06-02 18:41:38 +0200

    Fix Test Itineraire service et GetItineraire

--- src/test/java/fr/ul/miage/metronav/domain/service/ServiceItineraireTest.java
+++ src/test/java/fr/ul/miage/metronav/domain/service/ServiceItineraireTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ServiceItineraireTest {
+public class ServiceItineraireTest {
 
     @Test
     public void testcalculerItineraireRapide(){
